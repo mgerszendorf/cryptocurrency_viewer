c420341dd67edcad0bad268f93fae1640a936cae
Marek Gerszendorf <marek.gerszendorf@wp.pl>
2022-03-21 21:23:31 +0100

    Include NavigationBar to the App component and checking the window size for the mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./styles/App.css";
+
+import NavigationBar from "./components/NavigationBar";
+import "./styles/NavigationBar.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  // Checking the window size for the mobile version
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <NavigationBar width={width} />
+    </div>
+  );
 }
 
 export default App;
