2d52f44d88ca7d00def345f2a02342f75be86231
Marek Gerszendorf <marek.gerszendorf@wp.pl>
2022-03-25 21:33:29 +0100

    Change API

--- src/api/cryptocurrencyApi.ts
+++ src/api/cryptocurrencyApi.ts
@@ -1,13 +1,13 @@
 const options = {
   method: "GET",
   headers: {
-    "x-rapidapi-host": "crypto-ranking-data.p.rapidapi.com",
-    "x-rapidapi-key": "209c2fd62dmshe31c7cf56d8a671p198554jsnf88216643595",
+    "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+    "X-RapidAPI-Key": "209c2fd62dmshe31c7cf56d8a671p198554jsnf88216643595",
   },
 };
 
 export const getCryptoList = async () =>
   await fetch(
-    `https://crypto-ranking-data.p.rapidapi.com/market/list/1/100`,
+    "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0",
     options
   ).then((response) => response.json().catch((err) => console.error(err)));
