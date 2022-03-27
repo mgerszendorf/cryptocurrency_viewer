bd3198e85ee6e865c659da46b78daf3b4b2f4d12
Marek Gerszendorf <marek.gerszendorf@wp.pl>
2022-03-27 13:49:48 +0200

    Add getCryptoHistory in API

--- src/api/cryptocurrencyApi.ts
+++ src/api/cryptocurrencyApi.ts
@@ -11,3 +11,8 @@ export const getCryptoList = async () =>
     "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0",
     options
   ).then((response) => response.json().catch((err) => console.error(err)));
+
+export const getCryptoHistory = async (uuid: string, timePeriod: string) =>
+  await fetch(
+    `https://coinranking1.p.rapidapi.com/coin/${uuid}/history?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=${timePeriod}`, options
+  ).then((response) => response.json().catch((err) => console.error(err)));
