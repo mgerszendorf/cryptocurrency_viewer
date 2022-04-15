2721c6b4dab00f33a7367c19f9fc60b7a9f70141
Marek Gerszendorf <marek.gerszendorf@wp.pl>
2022-04-15 18:07:25 +0200

    Added interface IFavoriteCryptocurrenciesResponse

---
+++ src/interfaces/IFavoriteCryptocurrenciesResponse.ts
@@ -0,0 +1,9 @@
+export interface IFavoriteCryptocurrenciesResponse {
+  change: string;
+  iconUrl: string;
+  name: string;
+  price: string;
+  rank: number;
+  symbol: string;
+  uuid: string;
+}
