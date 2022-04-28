0c1dd0b8d9faaa5386533822010347cff94567c9
Marek Gerszendorf <marek.gerszendorf@wp.pl>
2022-04-28 22:58:21 +0200

    Fix cors problem

--- server/index.ts
+++ server/index.ts
@@ -14,6 +14,14 @@ app.use(helmet());
 app.use("/", express.static(path.join(__dirname, "static")));
 app.use(bodyParser.json());
 
+app.use(function (req: any, res: any, next: any) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 // MongoDB connection
 mongoose
   .connect(
